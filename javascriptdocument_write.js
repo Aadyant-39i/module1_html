fa7ecc9430a70f6b7ae077d60082feaea8dd965d
Aadyant-39i <nmadhupnb@gmail.com>
2026-05-13 12:47:02 +0530

    Add files via upload

---
+++ javascriptdocument_write.js
@@ -0,0 +1,2 @@
+var result = function (a, b) {return a * b;}
+        document.write(result(5, 8));
---
+++ javascriptfunction.js
@@ -0,0 +1,10 @@
+var b = 5;
+function myFunction() {
+    var a = 4;
+    document.getElementById("demo").innerHTML = b * a; 
+document.getElementById("demo1").innerHTML = a;
+document.getElementById("demo2").innerHTML = b;
+
+}
+
+
